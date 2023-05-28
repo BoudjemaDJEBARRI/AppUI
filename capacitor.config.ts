d7927d17ea68741fb49d19e0a66217d53e3777f9
Boudjema DJEBARRI <boudjema.djebari@gmail.com>
2023-05-28 10:29:40 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AppUI',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
